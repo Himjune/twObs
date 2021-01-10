c7fc607589593c07b71dce51af886eb53435379c
Ilja Poputnikov <himjune@mail.ru>
2021-01-10 05:06:19 +0300

    opti events strs

--- main.lua
+++ main.lua
@@ -1,19 +1,16 @@
 --message('My first addon2!');
 
+iEvent = 0;
 function AddEventStr(msg)
-    local i = 0;
-
-    while _G["EventStr" .. i] do
-        i = i + 1;
-    end
-
-    frame = CreateFrame('Button', "EventStr" .. i, _G["Events_scrollframe_container"], "EventStrTemplate");
+    local i = iEvent;
+    local frame = CreateFrame('Button', "EventStr" .. i, _G["Events_scrollframe_container"], "EventStrTemplate");
     if i > 0 then
         _G["EventStr" .. i]:SetPoint("TOPLEFT", _G["EventStr" .. i-1], "BOTTOMLEFT", 0, -2);
     else
         _G["EventStr" .. i]:SetPoint("TOPLEFT", _G["Events_scrollframe_container"], "TOPLEFT", 0, -10);
     end
-    _G["EventStr" .. i .. "Info"]:SetText(msg);
+    _G["EventStr" .. i .. "Info"]:SetText(i .. ") " .. msg);
+    iEvent = iEvent + 1;
     frame:Show();
 end
 
