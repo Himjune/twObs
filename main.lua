02ee5170b8bdcb1a7200c3b345ac098b11364099
Ilja Poputnikov <himjune@mail.ru>
2021-01-10 04:18:36 +0300

    posting messages to list

--- main.lua
+++ main.lua
@@ -1,2 +1,25 @@
 --message('My first addon2!');
+
+function AddEventStr(msg)
+    local i = 0;
+
+    while _G["EventStr" .. i] do
+        i = i + 1;
+    end
+
+    frame = CreateFrame('Button', "EventStr" .. i, _G["Events_scrollframe_container"], "EventStrTemplate");
+    if i > 0 then
+        _G["EventStr" .. i]:SetPoint("TOPLEFT", _G["EventStr" .. i-1], "BOTTOMLEFT", 0, -2);
+    else
+        _G["EventStr" .. i]:SetPoint("TOPLEFT", _G["Events_scrollframe_container"], "TOPLEFT", 0, -10);
+    end
+    _G["EventStr" .. i .. "Info"]:SetText(msg);
+    frame:Show();
+end
+
 TWObs_Frame:Show();
+AddEventStr("msg01");
+AddEventStr("msg02");
+AddEventStr("msg03");
+AddEventStr("msg04");
+AddEventStr("msg05");
